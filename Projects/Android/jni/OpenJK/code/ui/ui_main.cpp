d71812239c39b0a80b666b4628f30fe9a846a358
Simon <simonbrown77@googlemail.com>
2022-11-26 19:22:37 +0000

    Restore shiny saber by default

--- Projects/Android/jni/OpenJK/code/ui/ui_main.cpp
+++ Projects/Android/jni/OpenJK/code/ui/ui_main.cpp
@@ -4274,7 +4274,7 @@ void UI_GetVideoSetup ( void )
 	Cvar_Register ( NULL, "ui_r_picmip",				"0", CVAR_ROM );
 	Cvar_Register ( NULL, "ui_r_texturebits",			"0", CVAR_ROM );
 	Cvar_Register ( NULL, "ui_r_texturemode",			"0", CVAR_ROM );
-	Cvar_Register ( NULL, "ui_r_detailtextures",		"0", CVAR_ROM );
+	Cvar_Register ( NULL, "ui_r_detailtextures",		"1", CVAR_ROM );
 	Cvar_Register ( NULL, "ui_r_ext_compress_textures",	"0", CVAR_ROM );
 	Cvar_Register ( NULL, "ui_r_depthbits",				"24", CVAR_ROM );
 	Cvar_Register ( NULL, "ui_r_subdivisions",			"0", CVAR_ROM );
