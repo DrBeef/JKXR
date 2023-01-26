566da10ab6d8f392d2c30869ead19cae35eab089
Simon <simonbrown77@googlemail.com>
2023-01-26 22:06:18 +0000

    Build with optimisations in Release only
    Thanks to @Emawind for showing me how to do this!

--- Projects/Android/jni/OpenJK/Android.mk
+++ Projects/Android/jni/OpenJK/Android.mk
@@ -5,8 +5,13 @@ LOCAL_PATH := $(call my-dir)
 OPENXR_HMD = -DMETA_QUEST
 #OPENXR_HMD = -DPICO_XR
 
-JK3_BASE_CFLAGS = $(OPENXR_HMD) -O1 -DHAVE_GLES -DFINAL_BUILD -fexceptions  -Wall -Wno-write-strings -Wno-comment   -fno-caller-saves -fno-tree-vectorize -Wno-unused-but-set-variable -fvisibility=hidden
+JK3_BASE_CFLAGS = $(OPENXR_HMD) -DHAVE_GLES -DFINAL_BUILD -fexceptions  -Wall -Wno-write-strings -Wno-comment   -fno-caller-saves -fno-tree-vectorize -Wno-unused-but-set-variable -fvisibility=hidden
 JK3_BASE_CPPFLAGS =  -fvisibility-inlines-hidden -Wno-invalid-offsetof -fvisibility=hidden
+
+ifeq ($(NDK_DEBUG), 0)
+JK3_BASE_CFLAGS += -O3
+JK3_BASE_CPPFLAGS += -O3
+endif
 
 JK3_BASE_LDLIBS = 
 
@@ -18,6 +23,7 @@ JK3_BASE_C_INCLUDES :=   $(OPENJK_PATH)/lib $(JK3_CODE_PATH)/client $(JK3_CODE_P
 JK3_BASE_C_INCLUDES +=   $(LOCAL_PATH)/../../../../../../3rdParty/khronos/openxr/OpenXR-SDK/include
 JK3_BASE_C_INCLUDES +=   $(LOCAL_PATH)/../../../../../../3rdParty/khronos/openxr/OpenXR-SDK/src/common
 JK3_BASE_C_INCLUDES +=   $(JK3_CODE_PATH)/ $(OPENJK_PATH)/code/ $(OPENJK_PATH)/shared/ $(JK3_CODE_PATH)/ui $(OPENJK_PATH)/lib/gsl-lite/include
+
 
 # Jedi Outcast
 include $(OPENJK_PATH)/Android_gles_jo.mk
