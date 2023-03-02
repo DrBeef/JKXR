cb4c796f4b343560497be32228089e5102b12635
Simon <simonbrown77@googlemail.com>
2023-03-02 20:10:39 +0000

    remove JKA assets from EA build

--- make_z_vr_assets_pk3.bat
+++ make_z_vr_assets_pk3.bat
@@ -5,11 +5,4 @@ powershell Compress-Archive z_vr_assets/* z_vr_assets.zip
 rename z_vr_assets.zip z_vr_assets.pk3
 move z_vr_assets.pk3 assets/
 
-cd assets
-del z_vr_assets_jka.pk3
-cd ..
-powershell Compress-Archive z_vr_assets_jka/* z_vr_assets_jka.zip
-rename z_vr_assets_jka.zip z_vr_assets_jka.pk3
-move z_vr_assets_jka.pk3 assets/
-
 pause
