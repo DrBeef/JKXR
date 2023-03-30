3dd7833cd0d30a0f036e24a786e34b48a6357666
Simon <simonbrown77@googlemail.com>
2023-03-30 22:06:18 +0100

    Disable camera shake when charging a weapon's alt fire

--- Projects/Android/jni/OpenJK/code/cgame/cg_players.cpp
+++ Projects/Android/jni/OpenJK/code/cgame/cg_players.cpp
@@ -8580,6 +8580,8 @@ Ghoul2 Insert End
 				scale = 1.75f;
 			}
 
+			// No camera shake on weapon charge in VR
+			/*
 			if ( val < 0.0f )
 			{
 				val = 0.0f;
@@ -8593,6 +8595,7 @@ Ghoul2 Insert End
 			{
 				CGCam_Shake( val * val * 0.3f, 100 );
 			}
+			 */
 
 			val += Q_flrand(0.0f, 1.0f) * 0.5f;
 
--- Projects/Android/jni/OpenJK/code/cgame/cg_weapons.cpp
+++ Projects/Android/jni/OpenJK/code/cgame/cg_weapons.cpp
@@ -1454,6 +1454,7 @@ void CG_AddViewWeapon( playerState_t *ps )
 			scale = 1.75f;
 		}
 
+		/*
 		if ( val < 0.0f )
 		{
 			val = 0.0f;
@@ -1466,7 +1467,7 @@ void CG_AddViewWeapon( playerState_t *ps )
 		else
 		{
 			CGCam_Shake( val * val * 0.3f, 100 );
-		}
+		}*/
 
 		val += Q_flrand(0.0f, 1.0f) * 0.5f;
 
