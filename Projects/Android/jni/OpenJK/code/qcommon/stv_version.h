e488f5ce274a84ae5fc4a9cee1515b7f40483f4d
Simon <simonbrown77@googlemail.com>
2023-05-04 19:53:52 +0100

    Update version
    and remove an assert that was causing an issue in debug with the dark forces mod

--- Projects/Android/jni/OpenJK/code/qcommon/stv_version.h
+++ Projects/Android/jni/OpenJK/code/qcommon/stv_version.h
@@ -23,7 +23,7 @@ along with this program; if not, see <http://www.gnu.org/licenses/>.
 // Current version of the single player game
 #define VERSION_STRING_DOTTED		"1.0.1.1"
 
-#define JKXR_VERSION		"1.1.4"
+#define JKXR_VERSION		"1.1.5"
 
 #ifdef _DEBUG
 	#define	Q3_VERSION		"(debug)OpenJK: v" VERSION_STRING_DOTTED "  JKXR: " JKXR_VERSION
