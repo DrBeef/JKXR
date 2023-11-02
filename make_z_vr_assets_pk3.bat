59a79bdf84814133c2b72a3eb663fb2a93624d40
Simon <simonbrown77@googlemail.com>
2023-11-02 21:42:18 +0000

    Update make_z_vr_assets_pk3.bat

--- make_z_vr_assets_pk3.bat
+++ make_z_vr_assets_pk3.bat
@@ -1,9 +1,9 @@
 cd assets
-del z_vr_assets.pk3
+del z_vr_assets_jka.pk3
 cd ..
-powershell Compress-Archive z_vr_assets/* z_vr_assets.zip
-rename z_vr_assets.zip z_vr_assets.pk3
-move z_vr_assets.pk3 assets/
+powershell Compress-Archive z_vr_assets_jka/* z_vr_assets_jka.zip
+rename z_vr_assets_jka.zip z_vr_assets_jka.pk3
+move z_vr_assets_jka.pk3 assets/
 
 cd assets
 del z_vr_assets_jka.pk3
@@ -11,5 +11,12 @@ cd ..
 powershell Compress-Archive z_vr_assets_jka/* z_vr_assets_jka.zip
 rename z_vr_assets_jka.zip z_vr_assets_jka.pk3
 move z_vr_assets_jka.pk3 assets/
+
+cd assets
+del z_vr_assets_base.pk3
+cd ..
+powershell Compress-Archive z_vr_assets_base/* z_vr_assets_base.zip
+rename z_vr_assets_base.zip z_vr_assets_base.pk3
+move z_vr_assets_base.pk3 assets/
 
 pause
